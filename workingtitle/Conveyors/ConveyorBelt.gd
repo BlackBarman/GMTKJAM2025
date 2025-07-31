a41e62aef9f4d6cab1df59ae019e625d116fb499
sebastiano zezza <sebastiano.zezza@gmail.com>
2025-07-31 19:38:07 +0200

    created silest for conveyor belts
    created silest for conveyor belts

--- workingtitle/Conveyors/ConveyorBelt.gd
+++ workingtitle/Conveyors/ConveyorBelt.gd
@@ -12,27 +12,31 @@ enum Direction {
 	set(value):
 		direction = value
 		conveyor_force = get_direction_vector() 
+		
+@export var belt_speed = 20
 
 var conveyor_force: Vector2 
 var bodies_in_area: Array[RigidBody2D] = []
 
 func _ready():
 	# Ensure conveyor_force is initialized at startup
-	conveyor_force = get_direction_vector() * 100
+	conveyor_force = get_direction_vector() * belt_speed
 	connect("body_entered", _on_body_entered)
 	connect("body_exited", _on_body_exited)
 
 func _physics_process(delta):
 	for body in bodies_in_area:
 		if body and body.is_inside_tree():
-			body.apply_central_impulse(conveyor_force * delta)
+			body.apply_force(conveyor_force*belt_speed)
 
 func _on_body_entered(body):
 	if body is RigidBody2D:
 		bodies_in_area.append(body)
+		body.linear_velocity = conveyor_force.normalized() * body.linear_velocity.length()
 
 func _on_body_exited(body):
-	bodies_in_area.erase(body)
+	if body is RigidBody2D:
+		bodies_in_area.erase(body)
 
 func get_direction_vector() -> Vector2:
 	match direction:
